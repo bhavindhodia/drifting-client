cdf0b46cdad1a7987192897aea97cc95ae8e46b6
bhavindhodia <bhavindhodia13@gmail.com>
2022-02-07 13:06:10 +0530

    Changes axiosClient based on env var

--- src/services/axiosClient.tsx
+++ src/services/axiosClient.tsx
@@ -2,8 +2,14 @@ import axios from "axios";
 
 const axiosClient = axios.create();
 
-axiosClient.defaults.baseURL = "http://localhost:5000/v1/";
+const developmentBaseURL = "http://localhost:5000/v1/";
+const productionBaseURL = "https://drifting-server.herokuapp.com/v1/";
+//axiosClient.defaults.baseURL = "http://localhost:5000/v1/";
 //axiosClient.defaults.baseURL = "https://drifting-server.herokuapp.com/v1/";
+axiosClient.defaults.baseURL =
+  process.env.NODE_ENV === "development"
+    ? developmentBaseURL
+    : productionBaseURL;
 axiosClient.defaults.withCredentials = true;
 
 //All request will wait 2 seconds before timeout
