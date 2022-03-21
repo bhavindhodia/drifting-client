7a54a0fc80a8a7c4ffd98beedf3a32e34b9d3ba6
bhavindhodia <bhavindhodia13@gmail.com>
2022-03-21 10:56:54 +0530

    Fixed production_url as env

--- src/services/axiosClient.tsx
+++ src/services/axiosClient.tsx
@@ -4,14 +4,14 @@ const axiosClient = axios.create();
 
 const developmentBaseURL = "http://localhost:5000/v1/";
 const productionBaseURL =
-  "https://drifting-server-6oyc9.ondigitalocean.app/v1/";
+  process.env.REACT_APP_PRODUCTION_URL !== undefined
+    ? process.env.REACT_APP_PRODUCTION_URL
+    : "https://drifting-server-6oyc9.ondigitalocean.app/v1/";
 //axiosClient.defaults.baseURL = "http://localhost:5000/v1/";
 //axiosClient.defaults.baseURL = "https://drifting-server.herokuapp.com/v1/";
 axiosClient.defaults.baseURL =
   process.env.NODE_ENV === "development"
     ? developmentBaseURL
-    : process.env.REACT_APP_PRODUCTION_URL !== undefined
-    ? process.env.REACT_APP_PRODUCTION_URL
     : productionBaseURL;
 axiosClient.defaults.withCredentials = true;
 
