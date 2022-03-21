2f61989da10d4e5a0f740218f5e3b37787b2974f
bhavindhodia <bhavindhodia13@gmail.com>
2022-03-21 23:52:46 +0530

    Added HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { Spinner } from "@chakra-ui/spinner";
 import ProtectedRoutes from "components/ProtectedRoutes"; //Authenticated routes
 import PublicRoute from "components/PublicRoute";
